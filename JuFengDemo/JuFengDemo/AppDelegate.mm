ab5090b9a6b26a966c67a357f20de8d014a27994
MegatronCY <megatroncy@163.com>
2019-07-24 20:26:22 +0800

    工程配置

--- JuFengDemo/JuFengDemo/AppDelegate.mm
+++ JuFengDemo/JuFengDemo/AppDelegate.mm
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <FunSDK/FunSDK.h>
 
 @interface AppDelegate ()
 
@@ -18,6 +19,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     sleep(2);
+    
+    FUN_Init();
     return YES;
 }
 
